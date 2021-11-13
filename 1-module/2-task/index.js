c3f92402234dad1c4f1a3dc7160be28ad98dd353
neogen22 <49803670+neogen22@users.noreply.github.com>
2021-11-13 12:09:59 +0300

    Допишите функцию валидации имени пользователя

--- 1-module/2-task/index.js
+++ 1-module/2-task/index.js
@@ -10,7 +10,16 @@ function print(text) {
  * чтобы функция sayHello работала корректно
  */
 function isValid(name) {
-  // ваш код...
+  if (typeof name === "string") {
+    if (name.length < 4) {
+      return false
+    } else {
+      let re = /^(?=.*\s).*$/
+      return !re.test(name)
+    }
+  } else {
+    return false
+  }
 }
 
 function sayHello() {
