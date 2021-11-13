b748988e0f1a589c92dd33a459fdce4c28a23f34
neogen22 <49803670+neogen22@users.noreply.github.com>
2021-11-13 09:56:00 +0300

    Вычислить факториал

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,11 @@
 function factorial(n) {
-  // ваш код...
+  let product = 1
+  if (n === 0 || n === 1) {
+    return 1
+  } else {
+    for (let i = n; i >= 2; i--) {
+      product = product * i
+    }
+    return product
+  }
 }
