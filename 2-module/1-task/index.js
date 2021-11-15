9736c23bd7ee6872067952c9d0001173e51bcedc
neogen22 <49803670+neogen22@users.noreply.github.com>
2021-11-15 23:20:15 +0300

    Функция sumSalary(salaries)

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,22 @@
-function sumSalary(salaries) {
-  // ваш код...
+let salaries = {
+  John: Infinity,
+  Ann: 1600,
+  Pete: 1300,
+  month: 'December',
+  currency: 'USD',
+  isPayed: false
 }
+function sumSalary(salaries) {
+  let sum = 0
+  for (const key in salaries) {
+    if (isNaN(salaries[key])) {
+      continue
+    }
+    if (salaries[key] != Infinity && salaries[key] != -Infinity && typeof salaries[key] == "number") {
+      sum += salaries[key]
+    }
+  }
+  return sum
+}
+
+
