c46af1d986efb6be3d7c0cc4117761deda0423d8
neogen22 <49803670+neogen22@users.noreply.github.com>
2021-11-19 02:23:30 +0300

    Функция showSalary(users, age)

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,3 +1,12 @@
-function showSalary(users, age) {
-  // ваш код...
+function showSalary(data, age) {
+  let res = []
+  for (let i = 0; i < data.length; i++) {
+    if (data[i].age <= age) {
+      res.push(data[i].name + ", ")
+      res.push(data[i].balance)
+      res.push("\n")
+    }
+  }
+  res.pop()
+  return res.join("")
 }
