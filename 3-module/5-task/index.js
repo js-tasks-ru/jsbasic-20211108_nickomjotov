6441c41bdc4dc1668db3b242d738d4690e540729
neogen22 <49803670+neogen22@users.noreply.github.com>
2021-11-20 12:03:58 +0300

    Найти min/max в строке

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,9 @@
-function getMinMax(str) {
-  // ваш код...
+function getMinMax(inputData) {
+  let a = inputData.split(" ")
+    .filter(el => +el)
+    .map(el=>+el)
+  return {
+    min: Math.min(...a),
+    max: Math.max(...a)
+  }
 }
