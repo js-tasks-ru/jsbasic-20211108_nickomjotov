ab352b0bb6e65731cd27273c9c2e8d03bf57a1e6
neogen22 <49803670+neogen22@users.noreply.github.com>
2021-11-29 23:23:38 +0300

    Revert "Выделите ячейки по диагонали"
    This reverts commit 86fd928c2dd675ea9ca6b703516e6330619ca0ad.

--- 4-module/2-task/index.js
+++ 4-module/2-task/index.js
@@ -1,13 +1,3 @@
 function makeDiagonalRed(table) {
-  let element = document.querySelectorAll(`td`);
-  for (let i = 0; i < element.length; i++) {
-    if (i === 0 || i % 6 === 0) {
-      element[i].style.backgroundColor = 'red'
-    }
-  }
+  // ваш код...
 }
-
-
-
-
-
