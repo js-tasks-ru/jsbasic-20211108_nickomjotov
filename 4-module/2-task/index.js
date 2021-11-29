86fd928c2dd675ea9ca6b703516e6330619ca0ad
neogen22 <49803670+neogen22@users.noreply.github.com>
2021-11-29 23:20:28 +0300

    Выделите ячейки по диагонали

--- 4-module/2-task/index.js
+++ 4-module/2-task/index.js
@@ -1,3 +1,13 @@
 function makeDiagonalRed(table) {
-  // ваш код...
+  let element = document.querySelectorAll(`td`);
+  for (let i = 0; i < element.length; i++) {
+    if (i === 0 || i % 6 === 0) {
+      element[i].style.backgroundColor = 'red'
+    }
+  }
 }
+
+
+
+
+
