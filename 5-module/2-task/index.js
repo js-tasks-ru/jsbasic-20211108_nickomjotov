ba1a85287c7b427609c514b317544fb438b6a9a2
neogen22 <49803670+neogen22@users.noreply.github.com>
2021-12-07 22:51:24 +0300

    Занятие 5: Обработчики событий

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,12 @@
 function toggleText() {
-  // ваш код...
+  let hideText = document.querySelector("button")
+  hideText.onclick = function () {
+    if (document.getElementById("text").hidden === true) {
+      document.getElementById("text").hidden = false;
+    } else {
+      document.getElementById("text").hidden = true;
+    }
+  }
 }
+
+
--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,22 @@
 function initCarousel() {
-  // ваш код...
+  let rightArrow = document.querySelector(".carousel__arrow_right")
+  let leftArrow = document.querySelector(".carousel__arrow_left")
+  let transform = document.querySelector(".carousel__inner")
+  let trans = 0
+  leftArrow.style.display = 'none'
+  leftArrow.addEventListener("click", (event) => {
+    trans = trans + 500
+    if (trans === 0) {
+      leftArrow.style.display = 'none'
+    }
+    transform.style.transform = `translateX(${trans}px)`
+  })
+  rightArrow.addEventListener("click", (event) => {
+    leftArrow.style.display = ''
+    trans = trans - 500
+    if (trans === -1500) {
+      rightArrow.style.display = 'none'
+    }
+    transform.style.transform = `translateX(${trans}px)`
+  })
 }
